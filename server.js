93eea3ab8064ad4b401b310f392487fe1243e42b
Ali Haider <alihaiderx736@gmail.com>
2020-10-31 15:26:13 +0000

    update env

--- server.js
+++ server.js
@@ -68,6 +68,6 @@ app.delete("/messages/:id", function(request, response){
   })
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
